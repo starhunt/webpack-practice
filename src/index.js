719c279e13ec6b6c840299827dc7244053093871
Myeongryeol Kim <myeongryeolkim@Myeongryeolui-MacBookPro.local>
2020-03-14 23:22:36 +0900

    fileloader #1

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "normalize.css";
+// import "./index.css";
+import styles from "./index.css";
+import $ from "jquery";
+import slackImg from "./assets/slack.jpg";
+
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "Hello WebPack 3";
+
+  const imgElement = document.createElement("img");
+  imgElement.src = slackImg;
+
+  console.log(slackImg);
+  console.log(styles);
+
+  element.appendChild(imgElement);
+  element.classList = styles.helloWebpack;
+  return element;
+}
+
+document.body.appendChild(component());
+console.log($(`.${styles.helloWebpack}`).length);
+console.log(`IS_PRODUCTION_MODE: ${IS_PRODUCTION}`);
